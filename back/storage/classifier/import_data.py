fe8e347fe16afad07f7ce7b1573670bf1e88d077
welsondeivid <welsondemigod@gmail.com>
2024-01-17 13:37:00 -0300

    ♻️ refactor: importando dependencias só uma vez

--- back/storage/classifier/import_data.py
+++ back/storage/classifier/import_data.py
@@ -1,6 +1,17 @@
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-
 import subprocess
-subprocess.run(["python", "-m", "spacy", "download", "pt_core_news_sm"])
+import spacy
+import nltk
+
+def download_spacy_data():
+    try:
+        spacy.load("pt_core_news_sm")
+    except OSError:
+        subprocess.run(["python", "-m", "spacy", "download", "pt_core_news_sm"])
+
+def download_nltk_data():
+    nltk.download('stopwords')
+    nltk.download('punkt')
+
+if __name__ == "__main__":
+    download_spacy_data()
+    download_nltk_data()
