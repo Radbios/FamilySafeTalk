e4b0762928e427296848c9f7735fc8e4577e4a3f
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2024-01-20 14:16:30 -0300

    feat: Tela com header e footer que vai ser usada nas telas de chat, contatos e configs

---
+++ FamilySafeTalk_Front/src/screens/telas_config/Config_Responsavel/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import {Feather} from '@expo/vector-icons';
+import { Conversas } from '../Config_Responsavel/conversas';
+import { Contatos } from '../Config_Responsavel/contatos';
+import { Configs } from '../Config_Responsavel/config';
+//import { Image } from "react-native";
+const Tab = createBottomTabNavigator();
+
+export function BottomTabNavigator () {
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarLabelStyle: { fontSize: 14 },
+        tabBarItemStyle: { width: 100 },
+        tabBarStyle: { backgroundColor: '#a0c4ff' },
+        tabBarInactiveTintColor: 'black',
+        headerTitleAlign: "center",
+        headerStyle: { backgroundColor: '#a0c4ff'},
+        headerStatusBarHeight: 30
+      }}>
+        <Tab.Screen
+          name="Conversas"
+          component={Conversas}
+          options={{
+            tabBarIcon:({ size=150, color }) => <Feather name="message-square" size={size} color={color}/> 
+          }}
+        />
+
+
+        <Tab.Screen
+          name="Contatos"
+          component={Contatos}
+          options={{
+            tabBarIcon:({ size, color }) => <Feather name="users" size={size} color={color}/>
+          }} 
+        />
+
+
+        <Tab.Screen
+          name="Configurações"
+          component={Configs}
+          options={{
+            tabBarIcon:({ size, color }) => <Feather name="settings" size={size} color={color}/>
+          }} 
+        />
+
+      </Tab.Navigator>
+
+    </NavigationContainer>
+  );
+};
+
+export default BottomTabNavigator;
