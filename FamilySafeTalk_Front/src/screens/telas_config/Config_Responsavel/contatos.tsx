26c9d8e7973163a348800f06cae2b7008950c779
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2024-01-20 14:17:12 -0300

    feat: telas de configurações, conversas e contatos

---
+++ FamilySafeTalk_Front/src/screens/telas_config/Config_Responsavel/contatos.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+export function Contatos(){
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 32}}></View>
+    )
+}
---
+++ FamilySafeTalk_Front/src/screens/telas_config/Config_Responsavel/conversas.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+export function Conversas(){
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 32}}></View>
+    )
+}
