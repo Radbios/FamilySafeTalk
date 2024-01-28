6d7a077d21c8413dda9d2565af4f2b2dcaa9aa08
radbios <radbios@gmail.com>
2024-01-28 17:59:18 -0300

    bug: correção do bug (erro ao tentar adicionar um contato)

--- back/app/Http/Controllers/Api/ContactController.php
+++ back/app/Http/Controllers/Api/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -26,9 +27,17 @@ class ContactController extends Controller
      */
     public function store(Request $request)
     {
+        $contact = User::where('email', $request->email)->first();
+        if(!$contact)
+        {
+            return response()->json([
+                'status' => "error",
+                'message' => "Usuário não existe"
+            ]);
+        }
         $data = Contact::create([
             "user_id" => Auth::user()->id,
-            "contact_id" => $request->contact_id,
+            "contact_id" => $contact->id,
             "name" => $request->name,
             "is_blocked" => false
         ]);
