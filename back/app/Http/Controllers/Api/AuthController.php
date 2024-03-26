57a2b4a8bc0f982150b74f77aeeb5bce0b233084
Fabiano <radbios@gmail.com>
2024-03-26 06:55:57 -0300

    bug: login

--- back/app/Http/Controllers/Api/AuthController.php
+++ back/app/Http/Controllers/Api/AuthController.php
@@ -25,9 +25,9 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (! $user || ! Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+            return [
+                'error' => "error"
+            ];
         }
 
         return [
